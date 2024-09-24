534ee320342f97f285b33aa74373c0d5815623a8
si eun <tldms123455@naver.com>
2024-09-24 18:23:41 +0900

    first commit

---
+++ src/main/java/com/sini/blocking_extensions/BlockingExtensionsApplication.java
@@ -0,0 +1,13 @@
+package com.sini.blocking_extensions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockingExtensionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockingExtensionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sini/blocking_extensions/BlockingExtensionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sini.blocking_extensions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockingExtensionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
