c4e9f6ef491d4e3baa2c0b14abbf1f31cda440a5
si eun <tldms123455@naver.com>
2024-10-31 22:34:53 +0900

    :recycle: refactor : Modify api

--- src/main/java/com/sini/blocking_extensions/controller/CustomExtensionController.java
+++ src/main/java/com/sini/blocking_extensions/controller/CustomExtensionController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/custom-extension")
+@RequestMapping("/v1/custom-extensions")
 public class CustomExtensionController {
 
     private final CustomExtensionService customExtensionService;
--- src/main/java/com/sini/blocking_extensions/controller/FixedExtensionController.java
+++ src/main/java/com/sini/blocking_extensions/controller/FixedExtensionController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/extension")
+@RequestMapping("/v1/extensions")
 public class FixedExtensionController {
 
     private final FixedExtensionService fixedExtensionService;
