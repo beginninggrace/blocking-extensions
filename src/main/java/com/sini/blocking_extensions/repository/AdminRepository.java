4be1d98a3fd8f361f6942c49766aec9847981f49
si eun <tldms123455@naver.com>
2024-11-02 04:30:52 +0900

    :seedling: chore : admin setting

---
+++ src/main/java/com/sini/blocking_extensions/repository/AdminRepository.java
@@ -0,0 +1,8 @@
+package com.sini.blocking_extensions.repository;
+
+import com.sini.blocking_extensions.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+}
