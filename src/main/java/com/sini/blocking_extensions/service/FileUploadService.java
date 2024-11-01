5a1e0c1f1917ecdfa75e4e8fa6afa14558ebdf49
si eun <tldms123455@naver.com>
2024-11-02 00:48:51 +0900

    :sparkles: feat : Implement S3 upload

--- src/main/java/com/sini/blocking_extensions/service/FileUploadService.java
+++ src/main/java/com/sini/blocking_extensions/service/FileUploadService.java
@@ -1,7 +1,11 @@
 package com.sini.blocking_extensions.service;
 
+import java.io.IOException;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface FileUploadService {
 
-
+    String upload(MultipartFile auctionImage, String keyName) throws IOException;
 
 }
+
--- src/main/java/com/sini/blocking_extensions/service/S3Service.java
+++ src/main/java/com/sini/blocking_extensions/service/S3Service.java
@@ -1,6 +1,43 @@
 package com.sini.blocking_extensions.service;
 
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.sini.blocking_extensions.global.exception.custom.NotFoundBucketException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j(topic = "S3Service")
 public class S3Service implements FileUploadService {
 
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+    // final 왜?
+    private AmazonS3 amazonS3;
+
+    @Override
+    public String upload(MultipartFile file, String keyName) throws IOException {
+        ObjectMetadata objMeta = new ObjectMetadata();
+        objMeta.setContentType(file.getContentType());
+        objMeta.setContentLength(file.getInputStream().available());
+
+        try {
+            amazonS3.putObject(bucket, keyName, file.getInputStream(), objMeta);
+        } catch (AmazonS3Exception e) {
+            throw new NotFoundBucketException("존재하지 않는 버킷입니다.");
+        }
+
+        return "https://dccg5mv6uel89.cloudfront.net/" + keyName;
+    }
 
 }
