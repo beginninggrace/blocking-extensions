f8db4b3e8df1ed84aa73e93061509476005c1a8c
si eun <tldms123455@naver.com>
2024-11-02 00:39:37 +0900

    :recycle: refactor : Change String type to FileCreateResponse

--- src/main/java/com/sini/blocking_extensions/controller/FileController.java
+++ src/main/java/com/sini/blocking_extensions/controller/FileController.java
@@ -1,14 +1,13 @@
 package com.sini.blocking_extensions.controller;
 
+import com.sini.blocking_extensions.dto.FileCreateResponse;
 import com.sini.blocking_extensions.global.common.CommonResponse;
 import com.sini.blocking_extensions.service.FileService;
-import jakarta.validation.Valid;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,7 +19,7 @@ public class FileController {
     private final FileService fileService;
 
     @PostMapping()
-    public CommonResponse<String> saveFile(@RequestParam("file")MultipartFile file) throws IOException {
+    public CommonResponse<FileCreateResponse> saveFile(@RequestParam("file")MultipartFile file) {
         return CommonResponse.ok(fileService.saveFile(file));
     }
 
