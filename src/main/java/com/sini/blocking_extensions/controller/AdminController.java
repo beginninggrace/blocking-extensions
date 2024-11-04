aa609c3e087e742bf3c357697aa1d62e72ad6910
si eun <tldms123455@naver.com>
2024-11-04 23:04:08 +0900

    :sparkles: feat : Implement AdminSignup Controller

--- src/main/java/com/sini/blocking_extensions/controller/AdminController.java
+++ src/main/java/com/sini/blocking_extensions/controller/AdminController.java
@@ -1,12 +1,35 @@
 package com.sini.blocking_extensions.controller;
 
+import com.sini.blocking_extensions.dto.AdminRequest;
+import com.sini.blocking_extensions.dto.AdminResponse;
+import com.sini.blocking_extensions.dto.AdminSignupRequest;
+import com.sini.blocking_extensions.global.common.CommonResponse;
+import com.sini.blocking_extensions.service.AdminService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/admins")
+@RequestMapping("/v1/users/admins")
 public class AdminController {
 
+    private final AdminService adminService;
+
+    @PostMapping("/signup") @ResponseStatus(HttpStatus.CREATED)
+    public void signup(@Valid @RequestBody AdminSignupRequest adminSignupRequest) {
+        adminService.signup(adminSignupRequest);
+    }
+
+
+    @PostMapping("/login")
+    public CommonResponse<AdminResponse> login(@RequestBody AdminRequest adminRequest, HttpServletResponse response) {
+        return CommonResponse.ok(adminService.login(adminRequest, response));
+    }
 }
--- src/main/java/com/sini/blocking_extensions/entity/Admin.java
+++ src/main/java/com/sini/blocking_extensions/entity/Admin.java
@@ -1,5 +1,6 @@
 package com.sini.blocking_extensions.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,5 +20,14 @@ public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
 
 }
