2b605956ecd6bf7d2878c39312e03a0dc40dd5a0
si eun <tldms123455@naver.com>
2024-11-07 23:18:28 +0900

    :seedling: chore : AdminService setting

--- src/main/java/com/sini/blocking_extensions/service/AdminService.java
+++ src/main/java/com/sini/blocking_extensions/service/AdminService.java
@@ -1,5 +1,9 @@
 package com.sini.blocking_extensions.service;
 
+import com.sini.blocking_extensions.dto.AdminRequest;
+import com.sini.blocking_extensions.dto.AdminResponse;
+import com.sini.blocking_extensions.dto.AdminSignupRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,4 +15,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j(topic = "AdminService")
 public class AdminService {
 
+    public void signup(AdminSignupRequest adminSignupRequest) {
+
+    }
+
+    public AdminResponse login(AdminRequest adminRequest, HttpServletResponse response) {
+        return null;
+    }
 }
