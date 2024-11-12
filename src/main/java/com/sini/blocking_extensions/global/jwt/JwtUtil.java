99b14db46406c74d9a9f58661c019ac698093e84
si eun <tldms123455@naver.com>
2024-11-12 23:42:41 +0900

    :sparkles: feat : Implement JwtUtil

---
+++ src/main/java/com/sini/blocking_extensions/global/jwt/JwtUtil.java
@@ -0,0 +1,63 @@
+package com.sini.blocking_extensions.global.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Slf4j(topic = "JwtUtil")
+@Component
+public class JwtUtil {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+
+    private Key key;
+
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    public String createToken(Long adminId, String email) {
+        Date date = new Date();
+
+        long TOKEN_TIME = 60 * 60 * 1000;
+        return BEARER_PREFIX +
+            Jwts.builder()
+                .setSubject(email)
+                .claim("adminId", adminId)
+                .setExpiration(new Date(date.getTime() + TOKEN_TIME))
+                .signWith(key, signatureAlgorithm)
+                .compact();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public Claims getUserInfoFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+
+}
